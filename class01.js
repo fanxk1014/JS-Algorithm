949492d0d22652e838e0e8b3d8333ad0deb1e3a4
xiaokun <fanxk1014@foxmail.com>
2019-01-24 15:10:17 +0800

    提交练习

--- class01.js
+++ class01.js
@@ -1,3 +1,13 @@
 /**
  * 判断一组字符串是否为回文
+ * 是 返回true，不是 返回false
  */
+{
+    function testReverse(str) {
+        let newStr = str.split('').reverse().join('');
+        return newStr === str;
+    }
+
+    let str = 'abcdcba';
+    console.log(testReverse(str));
+}
