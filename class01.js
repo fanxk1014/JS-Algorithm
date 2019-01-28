a1fa752539c7d4d1354313aaa0b55a8ffa613301
xiaokun <fanxk1014@foxmail.com>
2019-01-28 23:51:46 +0800

    数组去重

--- class01.js
+++ class01.js
@@ -2,12 +2,27 @@
  * 判断一组字符串是否为回文
  * 是 返回true，不是 返回false
  */
-{
-    function testReverse(str) {
-        let newStr = str.split('').reverse().join('');
-        return newStr === str;
-    }
+// {
+//     function testReverse(str) {
+//         let newStr = str.split('').reverse().join('');
+//         return newStr === str;
+//     }
 
-    let str = 'abcdcba';
-    console.log(testReverse(str));
-}
+//     let str = 'abcdcba';
+//     console.log(testReverse(str));
+// }
+
+    function unique(arr) {  
+        let hashTable = {};
+        let data = [];debugger
+        for(let i=0,l=arr.length;i<l;i++) {
+        if(!hashTable[arr[i]]) {
+            hashTable[arr[i]] = true;
+            data.push(arr[i]);
+        }
+        }
+        return data
+    }
+    let arr=[0,2,4,6,8,4,5,0];
+
+    console.log(unique(arr));
